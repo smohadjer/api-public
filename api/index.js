b5ecbb3547ab64f11001bc1b1a1fed2e5d9c9ec0
Saeid Mohadjer <saeid@fastmail.fm>
2024-01-04 11:48:27 +0100

    adds post

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 import client from './_db.js';
+import { ObjectId } from 'mongodb';
 
 export default async (req, res) => {
     try {
@@ -7,14 +8,28 @@ export default async (req, res) => {
         const collection = database.collection('sample');
 
         if (req.method === 'GET') {
-			const data = await collection.find({}).toArray();
+			const data = await collection.find({}).limit(1000).toArray();
 			res.json(data);
         }
 
         if (req.method === 'POST') {
-			res.status(500).send({
-				message: 'post not supported yet'
-			});
+			const doc = req.body.doc;
+			const clone = structuredClone(doc);
+			delete clone._id;
+
+			if (doc._id) {
+				const query = {_id: new ObjectId(doc._id)};
+				const result = await collection.replaceOne(query, clone);
+				console.log(result.matchedCount, result.modifiedCount);
+				res.status(200).send({
+					message: 'document replaced'
+				});
+			} else {
+				const insertResponse = await collection.insertOne(doc);
+				res.status(200).send({
+					message: `new document with id ${insertResponse.insertedId} got inserted`
+				});
+			}
         }
     } catch (e) {
         console.error(e);
